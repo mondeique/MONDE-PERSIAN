bbd832b9465667b341d463035324e44f3e787af3
ChanYeongShin <cyshin@yonsei.ac.kr>
2019-09-14 13:33:14 +0900

    [ADDED] data-management server url pattern

--- data_management/urls.py
+++ data_management/urls.py
@@ -13,9 +13,36 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path
+from django.views.generic import TemplateView
+
+from category_data.views import *
+
+app_name = 'category'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/import/original-images', OriginalImageCreateAPIView.as_view(), name='import_image'),
+    path('api/home',HomeRetrieveAPIView.as_view(), name='home'),
+    path('api/boxing/<int:image_id>',BoxingRetrieveAPIView.as_view()),
+    path('api/colorlabeling/<int:image_id>',ColorLabelingRetrieveAPIView.as_view()),
+    path('api/shapelabeling/<int:image_id>',ShapeLabelingRetrieveAPIView.as_view()),
+    path('api/handlelabeling/<int:image_id>',HandleLabelingRetrieveAPIView.as_view()),
+    path('api/charmlabeling/<int:image_id>',CharmLabelingRetrieveAPIView.as_view()),
+    path('api/decolabeling/<int:image_id>',DecoLabelingRetrieveAPIView.as_view()),
+    path('api/patternlabeling/<int:image_id>',PatternLabelingRetrieveAPIView.as_view()),
+    path('api/boxing/assign',BoxingAssignAPIView.as_view()),
+    path('api/labeling/assign',LabelingAssignAPIView.as_view()),
+    path('api/box/<int:original_image_id>',BoxCreateUpdateAPI.as_view()),
+    path('api/colorlabeling/<int:cropped_image_id>',ColorLabelCreateUpdateAPI.as_view()),
+    path('api/shapelabeling/<int:cropped_image_id>',ShapeLabelCreateUpdateAPI.as_view()),
+    path('api/handlelabeling/<int:cropped_image_id>',HandleLabelCreateUpdateAPI.as_view()),
+    path('api/charmlabeling/<int:cropped_image_id>',CharmLabelCreateUpdateAPI.as_view()),
+    path('api/decolabeling/<int:cropped_image_id>',DecoLabelCreateUpdateAPI.as_view()),
+    path('api/patternlabeling/<int:cropped_image_id>',PatternLabelCreateUpdateAPI.as_view()),
+    path('api/image/delete/<int:original_image_id>',BoxingDestroyAPIView.as_view()),
+    path('api/cropimage/delete/<int:cropped_image_id>',LabelingDestroyAPIView.as_view()),
+    path('api/image/hold/<int:original_image_id>',BoxingHoldAPIView.as_view()),
+    path('import/',TemplateView.as_view(template_name='working_page/import_csv.html')),
+
 ]
