32dd61294148e15228a8fa6c2dd5d713c044bcdd
ChanYeongShin <cyshin@yonsei.ac.kr>
2019-09-16 22:39:26 +0900

    [ADDED] mysql settings.py

--- data_management/settings.py
+++ data_management/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'data_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'MONDE-data-server',
+        'USER': 'MONDEIQUE-data',
+        'PASSWORD': 'DataManage123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
