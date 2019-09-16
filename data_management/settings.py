6ff8deaa2c645dc059b413dc09e3460971284d6b
ChanYeongShin <cyshin@yonsei.ac.kr>
2019-09-17 00:00:33 +0900

    [ADDED] knox & authtoken settings.py

--- data_management/settings.py
+++ data_management/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'category_data.apps.CategoryDataConfig'
+    'category_data.apps.CategoryDataConfig',
+    'knox',
+    'rest_framework.authtoken'
 ]
 
 
